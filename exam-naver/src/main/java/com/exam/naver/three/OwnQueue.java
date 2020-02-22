a6fc52d020fbbd40e2b54622d0d9c4709f171795
郑周金 <zhengzhoujin@d-bigdata.com>
2020-02-22 11:34:05 +0800

    I didn't finish the third problem

---
+++ exam-naver/src/main/java/com/exam/naver/three/OwnQueue.java
@@ -0,0 +1,47 @@
+package com.exam.naver.three;
+
+/**
+ * description: 自定义队列
+ * author:zzj
+ * create:2020/2/22 11:23
+ */
+public class OwnQueue {
+    private int size;
+    private int[] arr;
+    private int rear;
+
+    public OwnQueue(int size) {
+        if (size < 0) {
+            System.out.println("队列容量不能小于0");
+        } else {
+            this.size = size;
+            arr = new int[size];
+            rear = -1;
+        }
+    }
+
+    public boolean isEmpty() {
+        return rear == -1;
+    }
+
+    public boolean isFull() {
+        return rear == size - 1;
+    }
+
+    public boolean push(int data) {
+        if (isFull()) {
+            try {
+                throw new RuntimeException("队列已满，不能添加");
+            } catch (Exception e) {
+                return false;
+            }
+        } else {
+            arr[++rear] = data;
+            return true;
+        }
+    }
+
+    public int pop() {
+        return arr[rear];
+    }
+}
